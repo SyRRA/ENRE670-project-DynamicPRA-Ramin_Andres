c579d176a60afc26a5d932a8f26b1c719c8fa7d7
Andrés Ruiz-Tagle P <42621686+AndresRTP@users.noreply.github.com>
2019-11-19 16:32:25 -0500

    Update dataset_construction.py

--- dataset_construction.py
+++ dataset_construction.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 import scipy.sparse as sp
 import datetime
 import time
+
 #%%
 # open dataset
 Folder = 'pkl datasets'
@@ -23,27 +24,55 @@ print("--- %s seconds ---" % (time.time() - start_time))
 #%%
 # Open datasets - timestamp is used in both x and y variables!
 # Crusher
-crusher_ = dataset_pkl_all[1]
+crusher_ = dataset_pkl_all[1].reset_index().drop('index', axis=1)
 crusher_x = dataset_pkl_all[1].iloc[:,:-2]
 crusher_y = dataset_pkl_all[1].iloc[:,[0,-2]]
 # Filter
-filter_ = dataset_pkl_all[4]
+filter_ = dataset_pkl_all[4].reset_index().drop('index', axis=1)
 filter_x = dataset_pkl_all[4].iloc[:,:-2]
 filter_y = dataset_pkl_all[4].iloc[:,[0,-2]]
 # Belt
-belt_ = dataset_pkl_all[0]
+belt_ = dataset_pkl_all[0].reset_index().drop('index', axis=1)
 belt_x = dataset_pkl_all[0].iloc[:,:-2]
 belt_y = dataset_pkl_all[0].iloc[:,[0,-2]]
 # Feeder 1
-feed1_ = dataset_pkl_all[2]
+feed1_ = dataset_pkl_all[2].reset_index().drop('index', axis=1)
 feed1_x = dataset_pkl_all[2].iloc[:,:-2]
 feed1_y = dataset_pkl_all[2].iloc[:,[0,-2]]
 # Feeder 2
-feed2_ = dataset_pkl_all[3]
+feed2_ = dataset_pkl_all[3].reset_index().drop('index', axis=1)
 feed2_x = dataset_pkl_all[3].iloc[:,:-2]
 feed2_y = dataset_pkl_all[3].iloc[:,[0,-2]]
 # Maintenance logs
 maint_logs = dataset_pkl_all[5]
+#%%
+################################################ Scaling data
+# 1) separate test and training (based on index) - last two frames - ix: [2018-12-04 21:04:00 :end] == 218059
+# code for computing index: ind = feed1_.index[feed1_['Time'] == pd.Timestamp(year=2018, month=12, day=4, hour=21, minute=4)]
+
+c_lis = [crusher_.set_index('Time').astype(float).interpolate(method='time', axis=0, limit_direction ='forward'),
+         filter_.set_index('Time').astype(float).interpolate(method='time', axis=0, limit_direction ='forward'),
+         belt_.set_index('Time').astype(float).interpolate(method='time', axis=0, limit_direction ='forward'),
+         feed1_.set_index('Time').astype(float).interpolate(method='time', axis=0, limit_direction ='forward'),
+         feed2_.set_index('Time').astype(float).interpolate(method='time', axis=0, limit_direction ='forward')]
+train = [comp.iloc[:218058,:] for comp in c_lis] 
+test = [comp.iloc[218058:,:] for comp in c_lis] 
+#%%
+# 2) scale data using robust scaler
+from sklearn.preprocessing import RobustScaler
+
+sc = [RobustScaler(quantile_range=(10.0, 90.0)), RobustScaler(quantile_range=(10.0, 90.0)), RobustScaler(quantile_range=(10.0, 90.0)),
+      RobustScaler(quantile_range=(10.0, 90.0)), RobustScaler(quantile_range=(10.0, 90.0))]
+
+sc_train = [pd.DataFrame(sc[i].fit_transform(train[i]),columns=train[i].columns,index=train[i].index) for i in range(5)]
+sc_test = [pd.DataFrame(sc[i].transform(test[i]),columns=test[i].columns,index=test[i].index) for i in range(5)]
+#%%
+# 3) Merge train and test - rrsft of the model
+crusher_ = pd.concat([sc_train[0],sc_test[0]])
+filter_= pd.concat([sc_train[1],sc_test[1]])
+belt_= pd.concat([sc_train[2],sc_test[2]])
+feed1_= pd.concat([sc_train[3],sc_test[3]])
+feed2_= pd.concat([sc_train[4],sc_test[4]])
 #%% Max frame size
 #arr = maint_logs['Mantencion'].values
 
@@ -51,19 +80,23 @@ maint_logs = dataset_pkl_all[5]
 #Aaa = Aa.apply(lambda x: x  / np.timedelta64(1,'m')).astype('int64') % (24*60)
 
 #%% Total number of failures
-li = [crusher_y, filter_y, belt_y, feed1_y, feed2_y]
-num = 0
-for i in li:
-    num += i['Detencion'].sum()
-    
-total_failures = [df['Detencion'].sum() for df in li]
+#li = [crusher_y, filter_y, belt_y, feed1_y, feed2_y]
+#num = 0
+#for i in li:
+#    num += i['Detencion'].sum()
+#    
+#total_failures = [df['Detencion'].sum() for df in li]
 
 #%%
-crusher_time = crusher_['Time']
+################################################ Frame sizes
+crusher_time = crusher_.index.to_series().reset_index(drop=True)
+#%%
+#crusher_time.reset_index()
+#%%.reset_index()#crusher_['Time'] # SEE THIS!
 crusher_delta = crusher_time.diff().fillna(0)
 delta = crusher_delta.apply(lambda x: x  / np.timedelta64(1,'m')).astype('int64') % (24*60)
 #%%
-new_time_df = pd.DataFrame({'Time':crusher_time,'delta':delta}).reset_index()
+new_time_df = pd.DataFrame({'Time':crusher_time,'delta':delta}).reset_index(drop=True)
 frames_out = []
 frames_in = []
 for i in range(len(new_time_df)-1):
@@ -77,27 +110,55 @@ for i in range(len(new_time_df)-1):
 # sum(len(li) for li in frames_out2)
 #%%
 frames_out2 = [ix for ix in frames_out if len(ix)!=0]
-#%%
-frames_out2[0] = pd.date_range(new_time_df['Time'][0], frames_out2[0], freq='2min', closed=None)
+#%% fill first datetimes
+#frames_out2[0] = pd.date_range(new_time_df['Time'][0], frames_out2[0], freq='2min', closed=None)
 
+#%%
+################################################ Fill Nans
+# First approach, use linear interpolation
+#crusher_.set_index('Time', inplace=True)
+#filter_.set_index('Time', inplace=True)
+#belt_.set_index('Time', inplace=True)
+#feed1_.set_index('Time', inplace=True)
+#feed2_.set_index('Time', inplace=True)
+#%%
+crusher_ = crusher_.astype(float).interpolate(method='time', axis=0, limit_direction ='forward')
+filter_ = filter_.astype(float).interpolate(method='time', axis=0, limit_direction ='forward')
+belt_ = belt_.astype(float).interpolate(method='time', axis=0, limit_direction ='forward')
+feed1_ = feed1_.astype(float).interpolate(method='time', axis=0, limit_direction ='forward')
+feed2_ = feed2_.astype(float).interpolate(method='time', axis=0, limit_direction ='forward')
 
 ################################################
 ################################################ until here everything is OK
 #%%
 # Generating TIME Windows - OK
-crusher_tw = [crusher_.set_index('Time').loc[frames] for frames in frames_out2]
-filter_tw = [filter_.set_index('Time').loc[frames] for frames in frames_out2]
-belt_tw = [belt_.set_index('Time').loc[frames] for frames in frames_out2]
-feed1_tw = [feed1_.set_index('Time').loc[frames] for frames in frames_out2]
-feed2_tw = [feed2_.set_index('Time').loc[frames] for frames in frames_out2]
+crusher_tw = [[crusher_.loc[frames].iloc[:,:-2], crusher_.loc[frames].iloc[:,-2]] for frames in frames_out2]
+filter_tw = [[filter_.loc[frames].iloc[:,:-2], filter_.loc[frames].iloc[:,-2]] for frames in frames_out2]
+belt_tw = [[belt_.loc[frames].iloc[:,:-2], belt_.loc[frames].iloc[:,-2]] for frames in frames_out2]
+feed1_tw = [[feed1_.loc[frames].iloc[:,:-2], feed1_.loc[frames].iloc[:,-2]] for frames in frames_out2]
+feed2_tw = [[feed2_.loc[frames].iloc[:,:-2], feed2_.loc[frames].iloc[:,-2]] for frames in frames_out2]
 
-#%% amount of NaNs with failure label - Being developed
-AaA = crusher_.dropna()
-AaAa = crusher_.drop(AaA.index)
-print(AaAa['Detencion'].sum())
+#%%
+def time_window_train(comp,window_len):
+    for i in comp:        
+        x = i[0]
+        y = i[1]
+        x_aug = []
+        y_aug = []
+        if y.isnull().sum()>0:
+            num_frames = len(x) - time_window
+            for frame in range(num_frames)
+            x_aug.append()
+            y_aug.append()
 
-################################################
+#%%##############################################
+################################################  # Turning dataframes to X and Y arrays - feed models (sampling frequency: 2min)
+
+
+
+#%%##############################################
 ################################################ from here everything is OK    - data description and plots
+
 #%%
 def plot_cdf(x,col,component='insert component name'):
     
@@ -132,7 +193,7 @@ def plot_cdf(x,col,component='insert component name'):
     
 #%%
 # crusher must be done in two setps crusher.iloc[:,:6] , crusher.iloc[:,4:]
-plot_cdf(crusher_,'Detencion','Crusher')
+plot_cdf(filter_,'Detencion','Filter')
 
 #%%
 # Define label thresholds
